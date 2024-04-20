fff292ae122b1b9e6a793bd5e60b293a0f0cc415
Deyan Kostadinov <Deyan.k.kostadinov@gmail.com>
2024-04-20 18:24:17 +0300

    turn `mix.exs` into a docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule AutoDocPackage.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/zen-dev-lab/auto_doc_package"
+  @homepage_url "https://auto-doc.fly.dev"
+
   def project do
     [
       app: :auto_doc_package,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Auto Doc",
+      source_url: @source_url,
+      homepage_url: @homepage_url
     ]
   end
 
@@ -21,8 +30,32 @@ defmodule AutoDocPackage.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      #{:jason, "~> 1.2"},
-      #{:httpoison, "~> 2.0"}
+      {:jason, "~> 1.2"},
+      {:httpoison, "~> 2.2"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    """
+    "Auto Doc" is a microservice used for generating API documentation based on OpenApiSpex format for Elixir projects.
+    The package "AutoDocPackage" is used as way to send/receive requests to/from the "Auto Doc" server
+    and generate the API Docs directories and files filled with data, nested inside the respected `/documentation` directory.
+
+    More information can be found at:
+    AutoDoc GitHub: #{@source_url}
+    AutoDoc Website: #{@homepage_url}
+    """
+  end
+
+  defp package() do
+    [
+      name: "auto_doc_packagee",
+      # These are the default files included in the package
+      files:
+        ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src),
+      licenses: ["CC-BY-NC-ND-4.0"],
+      links: %{"GitHub" => "https://github.com/zen-dev-lab/auto_doc_package"}
     ]
   end
 end
